350b87a948ac89615a699bcbedf0c32cc079d920
anna-balan <dp101292baa@gmail.com>
2023-11-02 22:09:35 +0100

    skills section done

--- rush.js
+++ rush.js
@@ -1,9 +1,55 @@
 
-$(document).ready(function() {
+// $(document).ready(function() {
+//     function animateProgressBar(progressBar, targetWidth) {
+//       var width = 0;
+//       var interval = 7; 
+
+//       var animation = setInterval(function() {
+//         if (width >= targetWidth) {
+//           clearInterval(animation);
+//         } else {
+//           width++;
+//           progressBar.width(width + "%");
+//           progressBar.text(width + "%");
+//         }
+//       }, interval);
+//     }
+
+//     animateProgressBar($("#progress-bar1"), 80);
+//     animateProgressBar($("#progress-bar2"), 60);
+//     animateProgressBar($("#progress-bar3"), 70);
+//     animateProgressBar($("#progress-bar4"), 50);
+//     animateProgressBar($("#progress-bar5"), 80);
+//     animateProgressBar($("#progress-bar6"), 85);
+//     animateProgressBar($("#progress-bar7"), 76);
+
+// //sections smooth navigation
+//     function scrollToMenuItem(target) {
+//         $('html, body').animate({
+//           scrollTop: $(target).offset().top
+//         }, 300);
+//       }
+//       $('#menu-item1').click(function(e) {
+//         e.preventDefault();
+//         scrollToMenuItem('#About'); 
+//       });
+    
+//       $('#menu-item2').click(function(e) {
+//         e.preventDefault();
+//         scrollToMenuItem('#Skills'); 
+//       });
+//       $('#menu-item3').click(function(e) {
+//         e.preventDefault();
+//         scrollToMenuItem('#Contact'); 
+//       });
+    
+//   });
+
+  $(document).ready(function() {
     function animateProgressBar(progressBar, targetWidth) {
       var width = 0;
-      var interval = 50; // The time interval between each step (in milliseconds)
-
+      var interval = 7; 
+  
       var animation = setInterval(function() {
         if (width >= targetWidth) {
           clearInterval(animation);
@@ -14,15 +60,59 @@ $(document).ready(function() {
         }
       }, interval);
     }
+  
+    // Function to check if #Skills section is in view
+    function isSkillsSectionInView() {
+      var skillsSection = $("#Skills");
+      var windowHeight = $(window).height();
+      var rect = skillsSection[0].getBoundingClientRect();
+      return (
+        rect.top >= 0 && rect.top <= windowHeight
+      );
+    }
+  
+    // Function to activate progress bars when the Skills section is in view
+    function activateProgressBars() {
+      if (isSkillsSectionInView()) {
+        animateProgressBar($("#progress-bar1"), 80);
+        animateProgressBar($("#progress-bar2"), 60);
+        animateProgressBar($("#progress-bar3"), 70);
+        animateProgressBar($("#progress-bar4"), 50);
+        animateProgressBar($("#progress-bar5"), 80);
+        animateProgressBar($("#progress-bar6"), 85);
+        animateProgressBar($("#progress-bar7"), 76);
+      }
+    }
+  
+    // Activate progress bars on page load
+    activateProgressBars();
+  
+    // Use scroll event listener to check when #Skills section is in view during scrolling
+    $(window).scroll(function() {
+      activateProgressBars();
+    });
 
-    // Animate the first progress bar to 75%
-    animateProgressBar($("#progress-bar1"), 75);
-
-    // Animate the second progress bar to 50%
-    animateProgressBar($("#progress-bar2"), 50);
+    //sections smooth navigation
+    function scrollToMenuItem(target) {
+        $('html, body').animate({
+          scrollTop: $(target).offset().top
+        }, 300);
+      }
+      $('#menu-item1').click(function(e) {
+        e.preventDefault();
+        scrollToMenuItem('#About'); 
+      });
+    
+      $('#menu-item2').click(function(e) {
+        e.preventDefault();
+        scrollToMenuItem('#Skills'); 
+      });
+      $('#menu-item3').click(function(e) {
+        e.preventDefault();
+        scrollToMenuItem('#Contact'); 
+      });
   });
-
-
+  
 
 
 
